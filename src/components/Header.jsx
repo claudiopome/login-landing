3960538a92221b5c92025f614375ea4109408a3f
claudiopome <claudiopome@gmail.com>
2024-01-16 18:00:32 +0100

    migrate the whole app to tailwind.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center mt-8 mb-16">
+    <header className="flex flex-col items-center mt-8 mb-8 md:mb-16 ">
       <img
         src={logo}
         alt="A canvas"
         className="mb-8 w-44 h-44 object-contain"
       />
-      <h1 className="text-4xl font-semibold tracking-widest uppercase text-amber-800 font-title">
+      <h1 className="text-4xl font-semibold tracking-widest uppercase text-amber-800 font-title md:text-4xl">
         ReactArt
       </h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
