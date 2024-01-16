4abdb236906701039468c90f796703bd75fcfe9a
claudiopome <claudiopome@gmail.com>
2024-01-16 16:48:37 +0100

    style img and heading.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,14 @@ import "./Header.css";
 export default function Header() {
   return (
     <header className="flex flex-col items-center mt-8 mb-16">
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+      <img
+        src={logo}
+        alt="A canvas"
+        className="mb-8 w-44 h-44 object-contain"
+      />
+      <h1 className="text-4xl font-semibold tracking-widest uppercase text-amber-800">
+        ReactArt
+      </h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
